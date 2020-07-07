fix nesterov accelerated gradient

mlfromscratch/deep_learning/activation_functions.py
@@ -8,7 +8,7 @@ class Sigmoid():
         return 1 / (1 + np.exp(-x))
 
     def gradient(self, x):
-        return self.__call__(x) * (1 - self.__call__(x))
+        return self(x) * (1 - self(x))
 
 class Softmax():
     def __call__(self, x):
@@ -16,15 +16,14 @@ class Softmax():
         return e_x / np.sum(e_x, axis=-1, keepdims=True)
 
     def gradient(self, x):
-        p = self.__call__(x)
-        return p * (1 - p)
+        return self(x) * (1 - self(x))
 
 class TanH():
     def __call__(self, x):
         return 2 / (1 + np.exp(-2*x)) - 1
 
     def gradient(self, x):
-        return 1 - np.power(self.__call__(x), 2)
+        return 1 - np.power(self(x), 2)
 
 class ReLU():
     def __call__(self, x):
@@ -51,7 +50,7 @@ class ELU():
         return np.where(x >= 0.0, x, self.alpha * (np.exp(x) - 1))
 
     def gradient(self, x):
-        return np.where(x >= 0.0, 1, self.__call__(x) + self.alpha)
+        return np.where(x >= 0.0, 1, self(x) + self.alpha)
 
 class SELU():
     # Reference : https://arxiv.org/abs/1706.02515,